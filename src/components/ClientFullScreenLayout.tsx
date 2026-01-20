4b577962051b6b2720bd71afec2d5bbacd45c646
leoxavier3914-arch <leoxavier3914@gmail.com>
2026-01-20 10:45:34 -0300

    Fix ClientAppShell to pass children

---
+++ src/components/ClientFullScreenLayout.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useLayoutEffect, type ReactNode } from "react";
+import { usePathname } from "next/navigation";
+
+import ClientMenu from "./ClientMenu";
+import styles from "./ClientFullScreenLayout.module.css";
+
+type ClientFullScreenLayoutProps = {
+  children: ReactNode;
+};
+
+const BODY_CLASS = "client-fullscreen";
+const DISABLE_PADDING_ROUTES = [
+  "/agendamentos",
+  "/procedimento",
+  "/meu-perfil",
+];
+
+export default function ClientFullScreenLayout({
+  children,
+}: ClientFullScreenLayoutProps) {
+  const pathname = usePathname();
+
+  useLayoutEffect(() => {
+    const body = document.body;
+    body.classList.add(BODY_CLASS);
+
+    return () => {
+      body.classList.remove(BODY_CLASS);
+    };
+  }, []);
+
+  const disableContentPadding = DISABLE_PADDING_ROUTES.some((route) =>
+    pathname === route || pathname?.startsWith(`${route}/`),
+  );
+
+  return (
+    <div className={styles.fullscreenWrapper}>
+      <ClientMenu disableContentPadding={disableContentPadding}>{children}</ClientMenu>
+    </div>
+  );
+}
